70d5198d7879a55a9af0607729acf1aeadfa9ba5
vhchuong1997 <vhchuong1997@gmail.com>
2021-07-25 21:33:00 +0800

    visualize throughput with pyshark

--- pyshark_visualize_throughput.py
+++ pyshark_visualize_throughput.py
@@ -5,9 +5,6 @@ import matplotlib.pyplot as plt
 
 capture = pyshark.FileCapture('broadcom.pcap')
 
-# last_packet = capture[-1:]
-# last_time = round(last_packet.frame_info.time_relative)
-
 time_list = []
 frlen_list = []
 sum_len = 0
@@ -17,8 +14,7 @@ for packet in capture:
 
 d = {'time':time_list,'frame length':frlen_list}
 df = pd.DataFrame(data=d)
-# last_packet = capture[-1:]
-# last_time = round(last_packet.frame_info.time_relative)
+
 final_time = round(df.iloc[-1:]['time'].values[0])
 print(final_time)
 
@@ -33,7 +29,7 @@ for i in range(1,final_time+1):
 
 dfi = pd.DataFrame(data={'time':time_range_list, 'throughput':num_packet_list})
 dfi.plot(x ='time', y='throughput', kind = 'line')
-# dfi.plot(x ='time', y='throughput', drawstyle = 'steps')
+
 plt.xlabel('time')
 plt.ylabel('throughput (bytes/s)')
 
